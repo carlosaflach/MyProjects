75eb8aec3a0c1f8601828d802534aa87cec0c69d
Carlos Afonso <carlos.flach15@gmail.com>
2022-02-13 11:16:05 -0300

    Changing the background color when player wins.

--- 01-Project-Guess-My-Number/script.js
+++ 01-Project-Guess-My-Number/script.js
@@ -8,10 +8,19 @@ let score = 20;
 document.querySelector('.check').addEventListener('click',function() {
   const guess = Number(document.querySelector('.guess').value);
 
+// When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = "⚠️ No Number!⚠️";
+
+    // When player wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎯 Correct Number!'
+    
+    document.querySelector('body').style.backgroundColor = '#60b347'
+
+    document.querySelector('.number').style.width = '30rem'
+    
+    // When guess is to high
   } else if (guess > secretNumber) {
     if(score > 1) {
       document.querySelector('.message').textContent = '⏫ Too high!!'
@@ -22,6 +31,8 @@ document.querySelector('.check').addEventListener('click',function() {
       document.querySelector('.score').textContent = score;
       document.querySelector('.message').textContent = '😢 You lost the game!'
     }
+
+    // When guess is to low
   } else if (guess < secretNumber) {
     if(score > 1) {
       document.querySelector('.message').textContent = '⏬ Too low!!'
