c19b31603c27dd798285ec111daa128337932a31
Carlos Afonso <carlos.flach15@gmail.com>
2022-02-13 11:34:34 -0300

    Implementing th highscore.

--- 01-Project-Guess-My-Number/script.js
+++ 01-Project-Guess-My-Number/script.js
@@ -4,6 +4,7 @@ const secretNumber = Math.trunc(Math.random() * 20) + 1;
 document.querySelector('.number').textContent = secretNumber;
 
 let score = 20;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click',function() {
   const guess = Number(document.querySelector('.guess').value);
@@ -19,6 +20,11 @@ document.querySelector('.check').addEventListener('click',function() {
     document.querySelector('body').style.backgroundColor = '#60b347'
 
     document.querySelector('.number').style.width = '30rem'
+
+    if(score > highscore) {
+      highscore = score
+      document.querySelector('.highscore').textContent = highscore;
+    }
 
     // When guess is to high
   } else if (guess > secretNumber) {
